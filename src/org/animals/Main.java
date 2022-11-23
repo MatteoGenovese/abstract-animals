cc07368d60cee290a54ed6690097a0a329d688b8
MatteoGenovese <matteo.genovese@icloud.com>
2022-11-23 09:37:41 +0100

    final push

--- src/org/animals/Main.java
+++ src/org/animals/Main.java
@@ -4,22 +4,47 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Eagle e = new Eagle();
-		Dog dg= new Dog();
-		Sparrow s = new Sparrow();
-		Dolphin dn = new Dolphin();
 		
+		Eagle e = new Eagle();
+		e.eat();
+		e.animalSound();
+		e.sleep();
+		
+		System.out.println("--------------------------------");
+		
+		Dog dg= new Dog();
+		dg.eat();
+		dg.animalSound();
+		dg.sleep();
+		
+		System.out.println("--------------------------------");
+		
+		Sparrow s = new Sparrow();
+		s.eat();
+		s.animalSound();
+		s.sleep();
+		
+		System.out.println("--------------------------------");
+		
+		Dolphin dn = new Dolphin();
+		dn.eat();
+		dn.animalSound();
+		dn.sleep();
+		
+		System.out.println("--------------------------------");
+		
+		doFly(e);
 		doFly(s);
 		
-
-		
-		
-		
+		doSwim(dn);
 	}
-	public static void doFly( IFlying Animal ) {
+	private static void doFly( IFlying animaleVolante ) {
 		
-
+		animaleVolante.fly();
+	}
+	private static void doSwim( ISwimming animaleNuotante ) {
 		
+		animaleNuotante.swim();
 	}
 
 }
